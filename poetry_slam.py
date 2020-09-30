44d69bc531b69115abc3289def42f080c67d97eb
Aldrin Brillante <aldrinbrillante@Aldrins-MacBook-Pro.local>
2020-09-29 18:09:40 -0700

    commits

--- poetry_slam.py
+++ poetry_slam.py
@@ -13,41 +13,31 @@ print("\n")
 # Create a function called lines_printed_backwards()
 # It should have 1 parameter called 'lines_list,' which is a 
 # list of strings containing lines of your poem
-
 # It should print the lines of the poem in reverse. 
 # Include the original line number at the beginning of each line
 
-def lines_printed_backwards(line_list):
-    #add_num = print(len(line_list))
-    #n = add_num
-    #for loop
-        # The reversed() function returns the reversed iterator of the given sequence.
-        # The list() constructor returns a list in Python.
-        # The open() function opens a file, and returns it as a file object.
-
-    for line in reversed(list(open("poem.txt"))):
-        print(line.rstrip())
-    # return temp variable line
-    return line
-
-# call function lines_printed_backwards
-lines_printed_backwards("poem.txt")
-
-
-# -------------------CO WORK CODE-----------------------------#
-
-def foo(ll):
-    num_lines = len(ll)
-    #print(ll)
-    ll = ll[::-1] #reverses the list
+def lines_printed_backward(line_list):
+    num_lines = len(line_list)
+    #print(line_list)
+    line_list = line_list[::-1] #reverses the list
 
     # 1 to num_lines
-    for i in range(len(ll)):
+    for i in range(len(line_list)):
         #print (num_lines - i)
-        #print(ll[i])
-        v = str(num_lines - i) + " " + ll[i]
+        #print(line_list[i])
+        v = str(num_lines - i) + " " + line_list[i]
         print(v)
 
 f = open('./poem.txt' , 'r') #opens the file
 #foo([1, 2, 3, 4]) 
-foo(f.read().splitlines()) # pass a list of lines to foo 
+lines_printed_backward(f.read().splitlines()) # pass a list of lines to foo 
+
+
+
+# Create a function called lines_printed_random():
+# have 1 parameter called lines_list, which is a list of strings containing lines of your poem
+# It should print the lines of your poem in randomly order. Repeats are ok, but make sure the 
+# number of lines printed is equal to the original lines in the poem (Line numbers do not need 
+# to be printed.) Hint: try using a loop and randint() from the random module.
+
+def lines_printe_random():
--- trial_error.py
+++ trial_error.py
@@ -1,12 +1,14 @@
 # Create a function called get_file_lines()
 # It should have 1 parameter called filename, which is a string of the filename.
 # It should return a list of strings containing the lines of the file.
-
 def get_file_lines(filename):
-    # create variable for open/read file_lines
     file_lines = open(filename, "r").read()
     return file_lines
-print(get_file_lines("poem.txt", "\n")
+
+print(get_file_lines("poem.txt"))
+print("\n")
+
+
 
 # Create a function called lines_printed_backwards()
 # It should have 1 parameter called 'lines_list,' which is a 
@@ -15,27 +17,37 @@ print(get_file_lines("poem.txt", "\n")
 # It should print the lines of the poem in reverse. 
 # Include the original line number at the beginning of each line
 
-def line_printed_backwards(line_list):
-    poem_dict_lines = open(line_list, "r").readLines()
-    reverse = ''
-    num_count = len(line_list)
-    for line in reversed(list(open("poem.txt"))):
-        num_count = num_count - 1
-        print(line.rstrip())
-    return reverse
-    
-
-
-
 # def lines_printed_backwards(line_list):
-#     poem_dict_lines = open(line_list, "r").readlines()
-#     reverse = ''
-#     num_count = len(line_list)
+#     #add_num = print(len(line_list))
+#     #n = add_num
+#     #for loop
+#         # The reversed() function returns the reversed iterator of the given sequence.
+#         # The list() constructor returns a list in Python.
+#         # The open() function opens a file, and returns it as a file object.
+
 #     for line in reversed(list(open("poem.txt"))):
-#         num_count = num_count - 1
-#         print(line.rstrip()) 
-#     return reverse
+#         print(line.rstrip())
+#     # return temp variable line
+#     return line
 
-print(lines_printed_backwards("poem.txt"))
+# # call function lines_printed_backwards
+# lines_printed_backwards("poem.txt")
 
 
+# -------------------CO WORK CODE-----------------------------#
+
+def lines_printed_backward(line_list):
+    num_lines = len(line_list)
+    #print(line_list)
+    line_list = line_list[::-1] #reverses the list
+
+    # 1 to num_lines
+    for i in range(len(line_list)):
+        #print (num_lines - i)
+        #print(line_list[i])
+        v = str(num_lines - i) + " " + line_list[i]
+        print(v)
+
+f = open('./poem.txt' , 'r') #opens the file
+#foo([1, 2, 3, 4]) 
+lines_printed_backward(f.read().splitlines()) # pass a list of lines to foo 
